23a92369155eff5e48543a31510454fe79851005
Oscar Mollé <scarmoll@gmail.com>
2020-09-28 12:53:40 +0200

    no message

--- app/src/androidTest/java/com/example/madlevel3task2/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/madlevel3task2/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.madlevel2task2
+package com.example.madlevel3task2
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.madlevel2task2", appContext.packageName)
+        assertEquals("com.example.madlevel3task2", appContext.packageName)
     }
 }
